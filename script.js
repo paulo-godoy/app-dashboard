3009a0468a18f39d167f19e025b8651db3fd7ece
paulo-godoy <pauloricardo.godoy@yahoo.com.br>
2019-07-03 23:06:13 -0300

    comentario

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(() => {
-	$('#documentacao').on('click', () => {
+    //requisição de paginas com ajax jquery para pagina documentação
+    $('#documentacao').on('click', () => {
        // $('#pagina').load('documentacao.html');
        $.get('documentacao.html', data => {
            $('#pagina').html(data);
        })
     })
-
+    //requisição de paginas com ajax jquery para pagina documentação
     $('#suporte').on('click', () => {
         //$('#pagina').load('suporte.html');
         $.get('suporte.html', data => {
