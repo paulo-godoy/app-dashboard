6d4a56db1399f645a32cfb36015b80604600cfbe
paulo-godoy <pauloricardo.godoy@yahoo.com.br>
2019-07-05 23:57:19 -0300

    ajustando o ajax

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 $(document).ready(() => {
-    //requisição de paginas com ajax jquery para pagina documentação
-    $('#documentacao').on('click', () => {
-       // $('#pagina').load('documentacao.html');
-       $.get('documentacao.html', data => {
-           $('#pagina').html(data);
-       })
-    })
-    //requisição de paginas com ajax jquery para pagina documentação
-    $('#suporte').on('click', () => {
-        //$('#pagina').load('suporte.html');
-        $.get('suporte.html', data => {
-            $('#pagina').html(data);
-        })
-    })
+	
+	$('#documentacao').on('click', () => {
+		//$('#pagina').load('documentacao.html')
+
+		/*
+		$.get('documentacao.html', data => { 
+			$('#pagina').html(data)
+		})
+		*/
+		$.post('documentacao.html', data => { 
+			$('#pagina').html(data)
+		})
+	})
+
+	$('#suporte').on('click', () => {
+		//$('#pagina').load('suporte.html')
+
+		/*
+		$.get('suporte.html', data => { 
+			$('#pagina').html(data)
+		})
+		*/
+		$.post('suporte.html', data => { 
+			$('#pagina').html(data)
+		})
+	})
+
+
+	//ajax
+	$('#competencia').on('change', e => {
+
+		let competencia = $(e.target).val()
+		
+		$.ajax({
+			type: 'GET',
+			url: 'app.php',
+			data: `competencia=${competencia}`, //x-www-form-urlencoded
+			dataType: 'json',
+			success: dados => { 
+				$('#numeroVendas').html(dados.numeroVendas)
+				$('#totalVendas').html(dados.totalVendas)
+			},
+			error: erro => { console.log(erro) }
+		})
+
+		//método, url, dados, sucesso, erro
+	})
 })
